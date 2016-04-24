cbefc876c9b381437f32d8257a857c218519b5d9
livisun999 <livisun999@gmail.com>
2016-04-24 12:26:54 +0700

    new data employee

--- config/app.php
+++ config/app.php
@@ -91,7 +91,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY'),
+    'key' => env('APP_KEY','Idgz1PE3zO9iNc0E3oeH3CHDPX9MzZe3'),
 
     'cipher' => 'AES-256-CBC',
 
--- config/database.php
+++ config/database.php
@@ -55,7 +55,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3309'),
+            'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'demo_laravel'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
